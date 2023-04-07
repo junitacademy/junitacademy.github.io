77f40a709bdaa08e24c5186739a8c8fd9524a557
Junit Academy <89267979+junitacademy@users.noreply.github.com>
2023-04-07 15:12:22 +0530

    Delete .svelte-kit directory

--- .svelte-kit/output/client/_app/immutable/chunks/2.235bef42.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.8566e76b.js";export{t as component};
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.772d77bf.js';
-export const imports = ["_app/immutable/entry/error.svelte.772d77bf.js","_app/immutable/chunks/index.019f7634.js","_app/immutable/chunks/singletons.2fe4f274.js"];
-export const stylesheets = [];
-export const fonts = [];
